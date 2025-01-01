75a3b2624867e5702bd14757e990805cded275a6
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-01 20:04:13 -0300

    Parte final concluída

--- js/ui.js
+++ js/ui.js
@@ -12,21 +12,20 @@ const ui = {
   limparFormulario() {
     document.getElementById("pensamento-form").reset();
   },
-
+  
   async renderizarPensamentos() {
     const listaPensamentos = document.getElementById("lista-pensamentos")
-    const mensagemVazia = document.getElementById("mensagem-vazia");
-    listaPensamentos.innerHTML = ''
+    const mensagemVazia = document.getElementById("mensagem-vazia")
+    listaPensamentos.innerHTML = ""
 
     try {
       const pensamentos = await api.buscarPensamentos()
-      pensamentos.forEach(ui.adicionarPensamentoNaLista)
       if (pensamentos.length === 0) {
-        mensagemVazia.style.display = "block";
+        mensagemVazia.style.display = "block"
       } else {
-        mensagemVazia.style.display = "none";
+        mensagemVazia.style.display = "none"
         pensamentos.forEach(ui.adicionarPensamentoNaLista)
-      }
+      } 
     }
     catch {
       alert('Erro ao renderizar pensamentos')
